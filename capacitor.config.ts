828b559f0ab21558c65381a27c56ac2bdcca9086
Samsamiri <samsamiri47@gmail.com>
2025-01-24 11:43:30 +0100

    first commit (project initialization)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'colocation.task.manager',
+  appName: 'colocation-task-manager',
+  webDir: 'www'
+};
+
+export default config;
