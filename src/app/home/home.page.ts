0c81e0787a4de0c31889afe681cde7d47aa4ead4
Samsamiri <samsamiri47@gmail.com>
2025-01-27 11:15:42 +0100

    feature : modification de la page profil avec la création de 4 profils. Ajout du component manage profil vide

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+
+interface Profile {
+  id: number
+  name: string
+  image?: string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
+  
+  profiles: Profile[] = [
+    { id: 1, name: "Alice", image: "https://api.dicebear.com/9.x/micah/svg?seed=Alice&hair=full&shirtColor=ffdfbf&backgroundColor=ffd5dc" },
+    { id: 2, name: "Bob", image: "https://api.dicebear.com/9.x/micah/svg?seed=Bob&hair=mrT&shirtColor=c0aede&backgroundColor=b6e3f4" },
+    { id: 3, name: "Charlie", image: "https://api.dicebear.com/9.x/micah/svg?seed=Charlie&hair=pixie&shirtColor=d1d4f9&backgroundColor=ffedef" },
+    { id: 4, name: "David", image: "https://api.dicebear.com/9.x/micah/svg?seed=David&hair=dougFunny&shirtColor=f4d150&backgroundColor=e0ddff" },
+  ];
+  
   constructor() {}
+
+  selectProfile(profile: Profile) {
+    console.log(`Profil sélectionné : ${profile.name}`)
+  }
+
+  manageProfiles() {
+    console.log("Gérer les profils")
+  }
 }
---
+++ src/app/manage-profile/manage-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-profile',
+  templateUrl: './manage-profile.component.html',
+  styleUrls: ['./manage-profile.component.scss'],
+  standalone: true,
+})
+export class ManageProfileComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
